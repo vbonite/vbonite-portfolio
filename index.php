1f43bb7f675f27e2e4679102a106ab82bd794289
Vlad Bonite <vlad.bonite@gmail.com>
2018-02-23 09:44:57 +0800

    New portfolio design

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+
+function getTitle() {
+}
+
+include 'partials/head.php';
+
+?>
+
+</head>
+<body>
+
+	<!-- main header -->
+	<!-- <?php include 'partials/main_header.php'; ?> -->
+
+	<!-- wrapper -->
+	<div class="container">
+	  	<h1 class="heading" data-target-resolver></h1>
+	</div>
+	<div class="portfolio">
+		<a href="https://vbonite.github.io/b3n-bonite-vlad/csp1/index.html#">Capstone Project 1</a>
+	</div>
+
+
+	<!-- main footer -->
+<!-- 	<?php include 'partials/main_footer.php'; ?>
+ -->
+<?php
+
+include 'partials/foot.php';
+
+?>
+
+</body>
+</html>
+
---
+++ partials/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
+	<title>Vlad Bonite - <?php getTitle(); ?></title>
+
+	<!-- Imports bootstrap -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+	<!-- Imports custom stylesheet -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Unica+One" rel="stylesheet">
+
+	<!-- Imports custom font for all pages -->
+	<link href="https://fonts.googleapis.com/css?family=Marmelad" rel="stylesheet">
